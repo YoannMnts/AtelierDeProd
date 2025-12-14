04568f85a1e2a48e6d143f471e224053b701bd1f
YoannMnts <yoann.montesinos@gmail.com>
2025-12-14 13:04:27 +0100

    Start of Jump

--- Assets/Scripts/Gameplay/Player/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 namespace Gameplay.Player
@@ -11,6 +12,7 @@ namespace Gameplay.Player
         public Vector3 Direction { get; private set; }
         public Vector3 TargetVelocity { get; private set; }
         public Vector3 CurrentVelocity { get; private set; }
+        public bool IsJumping { get; private set; }
         
         [Header("Components")]
         [field: SerializeField]
@@ -21,6 +23,19 @@ namespace Gameplay.Player
         [SerializeField] private float directionAlignDamping; 
         [SerializeField] private float acceleration; 
         [SerializeField] private float deceleration;
+        
+        [Header("Jump")]
+        [SerializeField] private float jumpForce;
+
+        private void OnEnable()
+        {
+            playerController.PlayerControls.JumpInput.performed += ApplyJumpForce;
+        }
+
+        private void OnDisable()
+        {
+            playerController.PlayerControls.JumpInput.performed -= ApplyJumpForce;
+        }
 
         private void Update()
         {
@@ -58,13 +73,23 @@ namespace Gameplay.Player
             var targetSpeed = lastTargetSpeed + delta * Time.deltaTime;
             targetSpeed = Mathf.Clamp(targetSpeed, 0, maxSpeed);
             
-            TargetVelocity = targetVelocity * targetSpeed;
+            TargetVelocity = targetVelocity * targetSpeed + Physics.gravity;
         }
 
         private void ComputeDirection()
         {
             var direction = playerController.PlayerControls.GetMovementInput();
             Direction = new Vector3(direction.x, 0.0f, direction.y).normalized;
+        }
+        
+        private void ApplyJumpForce(InputAction.CallbackContext context)
+        {
+            if (CharacterController.isGrounded && !IsJumping)
+            {
+                IsJumping = true;
+                CharacterController.Move(Vector3.up * jumpForce);
+            }
+            
         }
     }
 }
