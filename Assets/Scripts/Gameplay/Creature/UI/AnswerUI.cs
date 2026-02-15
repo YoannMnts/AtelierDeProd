bca7d9f61e33fc91471684126391328420601e84
YoannMnts <yoann.montesinos@gmail.com>
2026-02-15 13:38:06 +0100

    Answer can now AddOrRemove freindship when clicked

---
+++ Assets/Scripts/Gameplay/Creature/UI/AnswerUI.cs
@@ -0,0 +1,29 @@
+﻿using Ozkaal.Core.Datas.SymbolDatas;
+using Unity.Plastic.Newtonsoft.Json.Serialization;
+using UnityEngine;
+
+namespace Gameplay.Creature.UI
+{
+    public class AnswerUI : MonoBehaviour
+    {
+        public event Action OnButtonClick; 
+        
+        private Creature currentCreature;
+        
+        private AnswerData answerData;
+
+        public void Init(Creature creature, AnswerData answerData)
+        {
+            currentCreature = creature;
+            this.answerData = answerData;
+        }
+        
+        public void OnClick()
+        {
+            Debug.Log($"Add : {answerData.GainOrLossAmount} to friendship");
+            currentCreature.AddOrRemoveFriendship(answerData.GainOrLossAmount);
+            OnButtonClick?.Invoke();
+            currentCreature.StopTalking();
+        }
+    }
+}
