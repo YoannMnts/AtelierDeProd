d0ec999e63c756a7d31ea8343dd221febec1c3e3
YoannMnts <yoann.montesinos@gmail.com>
2026-03-03 20:40:06 +0100

    Camera now see through wall

--- Assets/Scripts/Gameplay/Player/PlayerCamera.cs
+++ Assets/Scripts/Gameplay/Player/PlayerCamera.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    
     [SerializeField, Range(0.1f, 1f)]
     private float zoomSpeed;
     
@@ -14,15 +16,48 @@ public class PlayerCamera : MonoBehaviour
     private float maxZoom;
     
     [SerializeField]
+    private LayerMask wallLayerMask;
+    
+    [SerializeField]
+    private Material wallMaterial;
+    
+    [SerializeField]
+    private int seeTroughDistance = 1000;
+
+    [SerializeField]
     private CinemachinePositionComposer positionComposer;
+    
+    [SerializeField]
+    private Camera cam;
+    
     
     private float velocity;
     private float smoothTime;
     
-
+    private int posID = Shader.PropertyToID("_Position");
+    private int sizeID = Shader.PropertyToID("Size");
+    
     private void OnValidate()
     {
         positionComposer.CameraDistance = minZoom;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+//        Gizmos.DrawLine(cam.transform.position, PlayerController.Instance.transform.position);
+    }
+
+    private void FixedUpdate()
+    {
+        var dir = (cam.transform.position - transform.position).normalized;
+        var ray = new Ray(transform.position, dir);
+
+        var value = Physics.Raycast(ray, out RaycastHit hit, seeTroughDistance, wallLayerMask) ? 1 : 0;
+        wallMaterial.SetFloat(sizeID, value);
+        
+        var view = cam.WorldToViewportPoint(transform.position);
+        wallMaterial.SetVector(posID, view);
     }
 
     private void OnEnable()
